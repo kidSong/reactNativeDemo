5b8ce4f05a810322b3cba6dba9beaee218beec8a
wangsong <QY00@wangsongdeMacBook-Pro.local>
2016-02-24 14:26:35 +0800

    继续练习

---
+++ xiecheng/common/search.js
@@ -0,0 +1,31 @@
+var React = require('react-native');
+var Util = require('./Utils');
+var {
+  StyleSheet,
+  Text,
+  View,
+  TabBarIOS,
+  TextInput
+  } = React;
+
+module.exports = React.createClass({
+  render: function(){
+    return (
+      <View style={styles.flex_1}>
+        <TextInput style={[styles.flex_1, styles.input]} {...this.props}/>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  flex_1:{
+    flex:1
+  },
+  input:{
+    borderWidth:Util.pixel,
+    height:40,
+    borderColor:'#DDDDDD',
+    paddingLeft:5
+  }
+});
--- xiecheng/index.ios.js
+++ xiecheng/index.ios.js
@@ -1,23 +1,98 @@
+
+'use strict';
+
 var React = require('react-native');
 var Index = require('./pages/index');
-
+var orderList = require('./pages/orderList');
 var {
+    TabBarIOS,
     NavigatorIOS,
     AppRegistry,
     StyleSheet,
-    View
+    View,
 } = React;
 
 var NV = React.createClass({
+
+     getInitialState: function() {
+      return {
+          selectedTab: 'order'
+      };
+  },
+
     render: function(){
         return(
-<NavigatorIOS
-    style={styles.container}
-    initialRoute={{
-        title: '首页',
-        component: Index,
-    }}
-/>
+
+    <TabBarIOS selectedTab = {this.state.selectedTab}>
+
+       <TabBarIOS.Item accessibilityLabel={"order"}
+            selected = {this.state.selectedTab === 'order'}
+            title = "订单"
+            name = "order"
+            icon = {{uri:'icon.png',isStatic:true}}
+             onPress={() => {
+            this.setState({
+              selectedTab: 'order'
+            });
+            }}
+       >
+       <NavigatorIOS
+            style={styles.container}
+            initialRoute={{
+            title: '首页',
+            component: Index,
+       }}
+        itemWrapperStyle={styles.navigator}
+      />
+      </TabBarIOS.Item>
+
+
+
+        <TabBarIOS.Item accessibilityLabel={"goods"}
+            selected = {this.state.selectedTab === 'goods'}
+            title = "商品"
+            name = "goods"
+            icon={{uri:'icon.png',isStatic:true}}
+            onPress={() => {
+            this.setState({
+              selectedTab: 'goods'
+            });
+            }}
+       >
+         <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: '商品',
+                    component: orderList,
+                }}
+                itemWrapperStyle={styles.navigator}
+            />
+       </TabBarIOS.Item>
+
+
+       <TabBarIOS.Item accessibilityLabel={"verify"}
+            selected = {this.state.selectedTab === 'verify'}
+            title = "验证"
+            name = "verify"
+            icon={{uri:'icon.png',isStatic:true}}
+            onPress={() => {
+            this.setState({
+              selectedTab: 'verify'
+            });
+          }}
+       >
+                   <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: '验证',
+                    component: Index,
+                }}
+                itemWrapperStyle={styles.navigator}
+            />
+       </TabBarIOS.Item>
+   
+    </TabBarIOS>
+
         );
     }
 });
@@ -25,7 +100,10 @@ var NV = React.createClass({
 var styles = StyleSheet.create({
     container: {
         flex: 1,
-    }
+    },
+     navigator: {
+    backgroundColor: '#E7EAEC'
+  }
 });
 
 
